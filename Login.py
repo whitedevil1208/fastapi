69a78110ad72d93b3153799ed524023ce234bb51
vikram balla <vikramballa@vikrams-MacBook-Air.local>
2025-07-26 00:37:23 +0530

    Updated my_script.py to add new calculation function

--- Login.py
+++ Login.py
@@ -9,8 +9,11 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 import re
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:Vikram%402025@localhost/luva"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
